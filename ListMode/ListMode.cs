0391b8f840ed568a014eb13706e3c4b3be994817
levabala <levabala@ya.ru>
2017-11-14 21:34:03 +0300

    Initial commit

---
+++ ListMode/ListMode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListMode
+{
+    public class ListMode
+    {
+        DateTime startTime, endTime;
+        object info;
+
+        public DateTime StartTime
+        {
+            get { return startTime; }
+        }
+        public DateTime EndTime
+        {
+            get { return endTime; }
+        }
+    }
+}
